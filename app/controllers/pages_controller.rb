f0db963d0f302b91dd46fb7bf838b352f1196e2e
Matthias Vitte <matthias@zenchef.com>
2016-08-30 18:36:57 +0200

    adding the times tamps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,7 @@
 class PagesController < ApplicationController
   def index
-    require 'net/http'
-
-url = URI.parse('https://eu.api.ovh.com/1.0/auth/time')
-req = Net::HTTP::Get.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
-  http.request(req)
-}
-puts res.body
-raise
+    time = Time.new()
+    timestamp = time.to_i
   end
 end
 
