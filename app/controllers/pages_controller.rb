b4143284b262b199718f3562bf4c72afa184f2fd
Matthias Vitte <matthias@zenchef.com>
2016-08-31 18:44:36 +0200

    adding the api keys

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   def index
     time = Time.new()
     timestemp = time.to_i
+
+    #calling the OVH gem
+    ovh = OVH::REST.new(apiKey, appSecret, consumerKey)
+
   end
 end
 
