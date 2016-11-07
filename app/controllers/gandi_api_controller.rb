4c1383caebea51db82bea90ebcf7d675d165ea71
Matthias Vitte <matthias@zenchef.com>
2016-11-07 19:11:01 +0100

    adding the create conatc method

--- app/controllers/gandi_api_controller.rb
+++ app/controllers/gandi_api_controller.rb
@@ -6,18 +6,39 @@ class GandiApiController < ApplicationController
     api.domain.transferin.available('mobicafe.fr') #transférer le NDD name to this controller
 
      #midifier les infos ci-dessous pour voir si le transfert fonctionne
+
+     contact_spec = {
+    'city' => 'Paris',
+    'country' => 'FR',
+    'email' => 'julien@zenchef.com',
+    'family' => 'Balmont',
+    'given' => 'Julien',
+    'password' => '0000000',
+    'phone' => '0688833732',
+    'streetaddr' => '120 rue Reaumur',
+    'type' => 1,
+    'orgname' => "SAS Zenchef"
+
+  }
+
+
+      api.contact.create(contact_spec)
+
+      api.contact.can_associate_domain('JB18182-GANDI', contact_spec) # the solution can be here => https://wiki.gandi.net/en/contacts/create
+
+      raise
     transfer_spec = {
-    'owner' => 'FLN123-GANDI',
-    'admin' => 'FLN123-GANDI',
-    'tech' => 'FLN123-GANDI',
-    'bill' => 'FLN123-GANDI',
+    'owner' => 'JB18182-GANDI',
+    'admin' => 'JB18182-GANDI',
+    'tech' => 'JB18182-GANDI',
+    'bill' => 'JB18182-GANDI',
     'nameservers' => ['a.dns.gandi.net', 'b.dns.gandi.net', 'c.dns.gandi.net'],
-    'authinfo' => 'xxx',
+    'authinfo' => 'X2pNNIqKdBTL',
     'duration' => 1}
 
     api.domain.transferin.proceed('mobicafe.fr', transfer_spec)
     api.operation.info(oper_id)
-    raise
+
   end
 
   def show
