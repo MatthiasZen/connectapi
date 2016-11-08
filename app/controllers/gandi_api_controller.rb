ae1fcf18ac0a7cb335bbee8e1de5a69416686670
Matthias Vitte <matthias@zenchef.com>
2016-11-08 19:02:13 +0100

    finding the gando hankle to transfert the ndd

--- app/controllers/gandi_api_controller.rb
+++ app/controllers/gandi_api_controller.rb
@@ -2,7 +2,7 @@ class GandiApiController < ApplicationController
   def index
 
     api = Gandi::Session.new(ENV["GandiapiKey"], env: :test) # Endpoint: https://rpc.gandi.net/xmlrpc/
-    api.domain.list
+
     api.domain.transferin.available('mobicafe.fr') #transférer le NDD name to this controller
 
      #midifier les infos ci-dessous pour voir si le transfert fonctionne
@@ -10,33 +10,32 @@ class GandiApiController < ApplicationController
      contact_spec = {
     'city' => 'Paris',
     'country' => 'FR',
-    'email' => 'julien@zenchef.com',
-    'family' => 'Balmont',
-    'given' => 'Julien',
+    'email' => 'matthias@zenchef.com',
+    'family' => 'Vitte',
+    'given' => 'Matthias',
     'password' => '0000000',
     'phone' => '0688833732',
     'streetaddr' => '120 rue Reaumur',
-    'type' => 1,
-    'orgname' => "SAS Zenchef"
+    'type' => 0,
 
   }
 
 
       api.contact.create(contact_spec)
 
-      api.contact.can_associate_domain('JB18182-GANDI', contact_spec) # the solution can be here => https://wiki.gandi.net/en/contacts/create
+      api.contact.can_associate_domain('MV22-GANDI', contact_spec)
 
-      raise
     transfer_spec = {
-    'owner' => 'JB18182-GANDI',
-    'admin' => 'JB18182-GANDI',
-    'tech' => 'JB18182-GANDI',
-    'bill' => 'JB18182-GANDI',
+    'owner' => 'MV22-GANDI',
+    'admin' => 'MV22-GANDI',
+    'tech' => 'MV22-GANDI',
+    'bill' => 'MV22-GANDI',
     'nameservers' => ['a.dns.gandi.net', 'b.dns.gandi.net', 'c.dns.gandi.net'],
     'authinfo' => 'X2pNNIqKdBTL',
     'duration' => 1}
 
-    api.domain.transferin.proceed('mobicafe.fr', transfer_spec)
+    api.domain.transferin.proceed('mobicafe.fr', transfer_spec) #le statut du code d'autorisation est en pending api.domain.available(['mobicafe.fr'])
+
     api.operation.info(oper_id)
 
   end
