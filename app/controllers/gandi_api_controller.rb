54d9db437fe88d188cedf75e28b500b06c2003f4
Matthias Vitte <matthias@zenchef.com>
2016-11-16 18:48:45 +0100

    making the gandi path

--- app/controllers/gandi_api_controller.rb
+++ app/controllers/gandi_api_controller.rb
@@ -3,39 +3,40 @@ class GandiApiController < ApplicationController
 
     api = Gandi::Session.new(ENV["GandiapiKey"], env: :test) # Endpoint: https://rpc.gandi.net/xmlrpc/
 
-    api.domain.transferin.available('mobicafe.fr') #transférer le NDD name to this controller
+    api.domain.transferin.available('mobicafe.fr', authinfo = 'X2pNNIqKdBTL') #transférer le NDD name to this controller ,'X2pNNIqKdBTL'
 
      #midifier les infos ci-dessous pour voir si le transfert fonctionne
 
      contact_spec = {
     'city' => 'Paris',
     'country' => 'FR',
-    'email' => 'matthias@zenchef.com',
-    'family' => 'Vitte',
+    'email' => 'julien@zenchef.com',
+    'family' => 'Balmont',
     'given' => 'Matthias',
     'password' => '0000000',
     'phone' => '0688833732',
     'streetaddr' => '120 rue Reaumur',
-    'type' => 0,
-
+    'type' => 1,
+    'orgname' => 'SAS Zenchef',
   }
+
+
 
 
       api.contact.create(contact_spec)
 
-      api.contact.can_associate_domain('MV22-GANDI', contact_spec)
+      api.contact.can_associate_domain('MV22-GANDI',  contact_spec)
 
     transfer_spec = {
     'owner' => 'MV22-GANDI',
     'admin' => 'MV22-GANDI',
     'tech' => 'MV22-GANDI',
     'bill' => 'MV22-GANDI',
-    'nameservers' => ['a.dns.gandi.net', 'b.dns.gandi.net', 'c.dns.gandi.net'],
-    'authinfo' => 'X2pNNIqKdBTL',
-    'duration' => 1}
+    }
+
 
     api.domain.transferin.proceed('mobicafe.fr', transfer_spec) #le statut du code d'autorisation est en pending api.domain.available(['mobicafe.fr'])
-
+ # voir les infos du reseller
     api.operation.info(oper_id)
 
   end
