e8a4266f4dd870faf0629de1f3dd77f1512688ac
Matthias Vitte <matthias@zenchef.com>
2016-12-29 19:00:13 +0100

    adding the key for develoeppement env for ovh

--- app/controllers/gandi_api_controller.rb
+++ app/controllers/gandi_api_controller.rb
@@ -1,7 +1,7 @@
 class GandiApiController < ApplicationController
 
   def index
-    api = Gandi::Session.new(ENV["GandiapiKey"], env: :test)
+    api = Gandi::Session.new(ENV["GandiapiKey"])
     @status_of_operation = api.operation.list({'type': 'domain_transfer_in'})
   end
 
@@ -9,7 +9,7 @@ class GandiApiController < ApplicationController
   end
 
   def update
-    api = Gandi::Session.new(ENV["GandiapiKey"], env: :test) # Endpoint: https://rpc.gandi.net/xmlrpc/
+    api = Gandi::Session.new(ENV["GandiapiKey"]) # Endpoint: https://rpc.gandi.net/xmlrpc/
 
     ndd = params['ndd']
     authcode = params['auth'].gsub(/["]/, '')
@@ -17,39 +17,44 @@ class GandiApiController < ApplicationController
     api.domain.transferin.available(ndd, authcode) #transférer le NDD name to this controller ,'X2pNNIqKdBTL'
 
 
+    # thi method if to create a contact but in teh test env whe still have 50 contacts
+     #contact_spec = {
+    #'city' => 'Paris',
+    #'country' => 'FR',
+    #'email' => 'jmatthias@zenchef.com',
+    #'family' => 'Vitte',
+    #'given' => 'Matthias',
+    #'password' => '0000000',
+    #'phone' => '0688833732',
+    #'streetaddr' => '120 rue Reaumur 75002',
+    #'type' => 0,
+    #'zip'=> "75002",
+    #}
 
-     contact_spec = {
-    'city' => 'Paris',
-    'country' => 'FR',
-    'email' => 'jmatthias@zenchef.com',
-    'family' => 'Vitte',
-    'given' => 'Matthias',
-    'password' => '0000000',
-    'phone' => '0688833732',
-    'streetaddr' => '120 rue Reaumur 75002',
-    'type' => 0,
-    'zip'=> "75002",
+
+    #api.contact.create(contact_spec)
+
+    association_spec = {
+      'domain' => ndd,
     }
 
-
-      api.contact.create(contact_spec)
-
-
-
-    api.contact.can_associate_domain('MV44-GANDI', contact_spec)
+    api.contact.can_associate_domain('JB18182-GANDI', association_spec)
 
     transfer_spec = {
-    'owner' => 'MV44-GANDI',
-    'admin' => 'MV44-GANDI',
-    'tech' => 'MV44-GANDI',
-    'bill' => 'MV44-GANDI',
-    #'authinfo' => authcode,
+    'owner' => 'JB18182-GANDI',
+    'admin' => 'JB18182-GANDI',
+    'tech' => 'JB18182-GANDI',
+    'bill' => 'JB18182-GANDI',
+    'authinfo' => authcode,
     }
+
 
 
     api.domain.transferin.proceed(ndd, transfer_spec)
 
-    api.operation.info(oper_id)
+
+    #api.operation
+    #api.operation.info(oper_id)
 
     redirect_to 'index'
 
